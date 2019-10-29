02dd4d137bc1ff44723bfd3493f791edaa7a5139
Ayrat Yamaltdinov <septikwar@gmail.com>
2019-10-29 23:54:04 +0300

    Create subscribe/unsubscribe buttons, subscriber/follower lists, mutual friends

--- frontend/modules/user/controllers/ProfileController.php
+++ frontend/modules/user/controllers/ProfileController.php
@@ -4,17 +4,86 @@ namespace frontend\modules\user\controllers;
 
 use frontend\models\User;
 use yii\web\Controller;
+use Yii;
+use Faker\Factory as Faker;
 
-class ProfileController extends Controller
-{
-    public function actionView($id)
-    {
-        return $this->render('view', ['user' => $this->findUser($id)]);
+class ProfileController extends Controller {
+
+    public function actionView($nickname) {
+        /*
+         *  @var $currentUser User;
+         */
+        $currentUser = Yii::$app->user->identity;
+        return $this->render('view', [
+            'user' => $this->findUser($nickname),
+            'currentUser' => $currentUser,
+        ]);
     }
-    public function findUser($id)
-    {
-        if ($user = User::find()->where(['id' => $id])->one()) {
+
+    /*
+     * @param string $nickname
+     * @return User
+     * @throws NotFoundHttpException
+     */
+
+    public function findUser($nickname) {
+        if ($user = User::find()->where(['nickname' => $nickname])->orWhere(['id' => $nickname])->one()) {
             return $user;
         }
     }
+
+    public function actionSubscribe($id) {
+        if (Yii::$app->user->isGuest) {
+            return $this->redirect(['/user/default/login']);
+        }
+        /* @var $currentUser User */
+        $currentUser = Yii::$app->user->identity;
+
+        /* @var $user User */
+        $user = $this->findUserById($id);
+        $currentUser->followUser($user);
+
+        return $this->redirect(['/user/profile/view', 'nickname' => $user->getNickname()]);
+    }
+    
+    public function actionUnsubscribe($id) {
+        if (Yii::$app->user->isGuest) {
+            return $this->redirect(['/user/default/login']);
+        }
+        /* @var $currentUser User */
+        $currentUser = Yii::$app->user->identity;
+
+        /* @var $user User */
+        $user = $this->findUserById($id);
+        $currentUser->unfollowUser($user);
+
+        return $this->redirect(['/user/profile/view', 'nickname' => $user->getNickname()]);
+    }
+
+    public function findUserById($id) {
+        if ($user = User::findOne($id)) {
+            return $user;
+        }
+        throw new NotFoundHttpException();
+    }
+
+//    public function actionGenerate() {
+//        $faker = Faker::create();
+//
+//        
+//        for ($i = 0; $i < 1000; $i++) {
+//            $user = new User([
+//                'username' => $faker->name,
+//                'email' => $faker->email,
+//                'about' => $faker->text(200),
+//                'nickname' => $faker->regexify('[A-Za-z0-9_]{5,15}'),
+//                'auth_key' => Yii::$app->security->generateRandomString(),
+//                'password_hash' => Yii::$app->security->generateRandomString(),
+//                'created_at' => $time = time(),
+//                'updated_at' => $time,
+//            ]);
+//            $user->save(false);
+//        }
+//    }
+
 }
--- frontend/modules/user/views/profile/view.php
+++ frontend/modules/user/views/profile/view.php
@@ -1,1 +1,80 @@
-<?php print_r($user); ?>
+<?php
+/* @var $this yii\web\View */
+/* @var $user frontend\models\User */
+/* @var $currentUser frontend\models\User */
+
+use yii\helpers\Url;
+use yii\helpers\Html;
+use yii\helpers\HtmlPurifier;
+?>
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+            <h3><?= Html::encode($user->username); ?></h3>
+            <p><?= HtmlPurifier::process($user->about); ?></p>
+            <hr>
+        </div>
+    </div>
+    <div class="row" style="margin: 10px -15px;">
+        <div class="col-xs-12">
+            <h4>Frinds, who are also following <?= Html::encode($user->username); ?></h4>
+
+                <?php foreach ($currentUser->getMutualsSubscriptionsTo($user) as $item) : ?>
+                <a href="<?= Url::to(['/user/profile/view', 'nickname' => ($item['nickname']) ? $item['nickname'] : $item['id']]); ?>">
+                <?= Html::encode($item['username']); ?>
+                </a>
+<?php endforeach; ?>
+        </div>
+        <div class="col-xs-12">
+            <h2>Profile subscribers & followers:</h2>
+            <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#subscribers">
+                Subscriptions: <?= $user->countSubscribers(); ?>
+            </button>
+            <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#followers">
+                Followers: <?= $user->countFollowers(); ?>
+            </button>
+            <hr>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-xs-12">
+            <a href="<?php echo Url::to(['/user/profile/subscribe', 'id' => $user->getId()]); ?>" class="btn btn-info">Subscribe</a>
+            <a href="<?php echo Url::to(['/user/profile/unsubscribe', 'id' => $user->getId()]); ?>" class="btn btn-info">Unsubscribe</a>
+        </div>
+    </div>
+</div>
+
+
+
+<div class="modal fade" id="subscribers" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="myModalLabel">Subscribers</h4>
+            </div>
+            <div class="modal-body">
+                <?php foreach ($user->getSubscriptions() as $subscribers) : ?>
+                    <a href="<?= Url::to(['/user/profile/view', 'nickname' => ($subscribers['nickname']) ? $subscribers['nickname'] : $subscribers['id']]); ?>"><?= Html::encode($subscribers['username']); ?></a><br>
+<?php endforeach; ?>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="modal fade" id="followers" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="myModalLabel">Subscribers</h4>
+            </div>
+            <div class="modal-body">
+                <?php foreach ($user->getFollowers() as $followers) : ?>
+                    <a href="<?= Url::to(['/user/profile/view', 'nickname' => ($followers['nickname']) ? $followers['nickname'] : $followers['id']]); ?>"><?= Html::encode($followers['username']); ?></a><br>
+<?php endforeach; ?>
+            </div>
+        </div>
+    </div>
+</div>
